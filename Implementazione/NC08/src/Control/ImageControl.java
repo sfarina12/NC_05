62c39ee98e87096c8669c5b73d0c1a29a2490965
Simone Farina <35815576+sfarina12@users.noreply.github.com>
2021-12-30 15:18:28 +0100

    update dei control

---
+++ Implementazione/NC08/src/Control/ImageControl.java
@@ -0,0 +1,57 @@
+package Control;
+
+import Bean.ProdottoBean;
+import Model.ProdottoModelDm;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet che controlla il caricamento delle immagini per le jsp.
+ *
+ * @author Farina Simone
+ */
+@WebServlet("/ImageControl")
+public class ImageControl extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+  private final ProdottoModelDm prodottoModelDm = new ProdottoModelDm();
+  
+  public ImageControl() {
+    super(); 
+  }
+
+  public void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws ServletException, IOException {
+
+    String id = (String) request.getParameter("isbn");
+
+    if (id != null) {
+      byte[] bt;
+
+      try {        
+        ProdottoBean pb = (ProdottoBean) prodottoModelDm.doQuery("doRetrieveByKey", id);
+        bt = pb.getCopertina();
+        
+        ServletOutputStream out = response.getOutputStream();
+
+        if (bt != null) {
+          out.write(bt);
+          response.setContentType("image/jpg");
+        }
+        
+        out.close();
+      } catch (Exception e) {
+        System.out.println("error: " + e.getMessage());
+      }
+    }
+  }
+
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    doGet(request, response);
+  }
+}
---
+++ Implementazione/NC08/src/Control/ProdottoControl.java
@@ -0,0 +1,61 @@
+package Control;
+
+import Bean.ProdottoBean;
+import Model.ProdottoModelDm;
+import java.io.IOException;
+import java.sql.SQLException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Implementazione servlet ProdottoControl.
+ *
+ * @author Farina Simone
+ */
+@WebServlet("/Prodotto")
+public class ProdottoControl extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+
+  /**
+     * Costruttore vuoto.
+     *
+     * @see HttpServlet#HttpServlet()
+     */
+  public ProdottoControl() {
+    super();
+  }
+
+  /**
+   *  Metodo doGet().
+   *
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    String id = request.getParameter("isbn");
+    ProdottoBean prodotto = new ProdottoBean();
+    try {
+      prodotto = (ProdottoBean) ProdottoModelDm.doQuery("doRetrieveByKey", id);
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    request.setAttribute("prodotto", prodotto);
+
+    RequestDispatcher requestDispatcher = request.getRequestDispatcher("/Prodotto.jsp");
+    requestDispatcher.forward(request, response);
+  }
+
+  /**
+   * Metodo doPost().
+   *
+   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+       throws ServletException, IOException {
+    doGet(request, response);
+  }
+}
